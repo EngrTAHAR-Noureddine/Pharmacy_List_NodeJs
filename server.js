0dbf9e3df083e4a0d3c68529dfe71c7f58683515
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-01-09 14:01:32 +0100

    remove comments

--- server.js
+++ server.js
@@ -21,16 +21,6 @@ mongoose.connect(db)
 app.use('/api/user',user);
 app.use('/api/consultation',consultation);
 
-/*
-if(process.env.NODE_ENV === 'production'){
-    //Set static folder
-    app.use(express.static('client/build'));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    });
-}
-*/
-
 
 const port = 5000;
 
