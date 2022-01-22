f719c36c1d4643ebecb2ad5afb987d30447118e9
EngrTAHAR-Noureddine <engr.tahar.noureddine@gmail.com>
2022-01-22 17:20:54 +0100

    1- add /login to post email and password to return user information 2- refactor return of get all user to get just pharmacies

--- routes/api/User.js
+++ routes/api/User.js
@@ -3,7 +3,29 @@ const router=express.Router();
 const User = require('../../models/User');
 
 router.get('/',(req,res)=>{
-    User.find().then(user=>res.json(user))
+
+    User.find().then(user=>{
+        let list = user.filter(u => u.typeUser === "Pharmacy");
+        list.forEach(element => element.passwordUser = null);
+        res.json(list)
+    })
+});
+
+router.post('/login',(req,res)=>{
+    User.find().then(user=>{
+        //console.log(user);
+
+        var findedUser = null;
+        if(user){
+        for (let i = 0; i < user.length; i++){
+            if(user[i].emailUser === req.body.emailUser && user[i].passwordUser === req.body.passwordUser){
+                //console.log(user[i])
+                findedUser = user[i]
+            }
+        }
+        }
+        res.json(findedUser)
+    })
 });
 
 router.post('/',(req,res)=>{
